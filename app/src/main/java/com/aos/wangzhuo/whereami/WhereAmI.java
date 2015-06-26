90f7dd608fe123d7c32e6dd69e3589bcda95f120
Kevin <kevinwang@ideaculture.com>
2015-06-26 14:36:31 +0800

    WhereAmI commit 2015/06/26 14:36

--- app/src/main/java/com/aos/wangzhuo/whereami/WhereAmI.java
+++ app/src/main/java/com/aos/wangzhuo/whereami/WhereAmI.java
@@ -1,7 +1,9 @@
 package com.aos.wangzhuo.whereami;
 
 import android.content.Context;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,8 +11,32 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.util.zip.CRC32;
+
 
 public class WhereAmI extends ActionBarActivity {
+
+    private final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            updateWithNewLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +47,21 @@ public class WhereAmI extends ActionBarActivity {
         String svcName = Context.LOCATION_SERVICE;
         locationManager = (LocationManager) getSystemService(svcName);
 
-        String provider = LocationManager.GPS_PROVIDER;
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(true);
+        String provider = locationManager.getBestProvider(criteria, true);
+
+//        String provider = LocationManager.GPS_PROVIDER;
         Location location = locationManager.getLastKnownLocation(provider);
 
         updateWithNewLocation(location);
+
+        locationManager.requestLocationUpdates(provider, 2000, 10, locationListener);
     }
 
     private void updateWithNewLocation(Location location) {
